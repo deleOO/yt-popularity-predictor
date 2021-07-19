3fb1cfc3549bd858cc9929b35b1ad2f5836febc3
cecilia-sartori <cecilia.sartori@studenti.unitn.it>
2021-07-19 23:25:47 +0200

    videos&channel metadata cleaning

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,8 +1,29 @@
 import json
+import pickle
+
+subset = "val"
+dataset = pickle.load(open("parsed_%s.pkl" % subset, "rb"))
+
+key_id = []
+value = []
+
+for x in dataset['videos']:
+    key_id.append(x)
+    value.append(dataset['videos'].get(x))
 
 with open('videos_metadata.json') as f:
     data = json.load(f)
-    
+
+video_id = []
+
+for i in range(len(data)):
+    video_id.append(data[i]['video_id'])
+
+# dict update with labels from yt8m
+
+for x, i in zip(video_id, range(len(data))):
+    data[i].update({'labels': dataset['videos'].get(x)})
+
 #convert uploadDate from string to datetime
     
 for i in range(len(data)):
