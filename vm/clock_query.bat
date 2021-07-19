432e074e63fb39bfe08559f9252bad06d8f6228f
bdt-2021-vm\ivandeleonardis <ivan.deleonardis@gmail.com>
2021-07-19 23:23:05 +0100

    batch file to be executed by task scheduler

---
+++ vm/clock_query.bat
@@ -0,0 +1,2 @@
+"C:\Users\ivandeleonardis\Documents\bdt_project\.venv\Scripts\python.exe" "C:\Users\ivandeleonardis\Documents\bdt_project\clock_query.py"
+pause
---
+++ vm/data_enrichment_trending.bat
@@ -0,0 +1,2 @@
+"C:\Users\ivandeleonardis\Documents\bdt_project\.venv\Scripts\python.exe" "C:\Users\ivandeleonardis\Documents\bdt_project\data_enrichment_trending.py"
+pause
